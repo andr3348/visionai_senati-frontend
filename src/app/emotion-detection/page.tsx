d1038e91a8b2622601c7a054c5f8056a185e0aa4
andree <andree1211val@gmail.com>
2025-10-12 21:03:57 -0500

    feature(emotion-detection): emotion detection layout

---
+++ src/app/emotion-detection/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+
+const EmotionDetection = () => {
+  return (
+    <div className="flex flex-col items-center h-full justify-center gap-6 p-6">
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-8 size-full">
+        {/* Camera Section */}
+        <div className="flex-2">
+          <div className="bg-gray-200 h-96 rounded-lg flex items-center justify-center">
+            <p>Camera will go here</p>
+          </div>
+        </div>
+
+        {/* Predictions Section */}
+        <div className="flex-1 sm:h-full">
+          <div className="bg-slate-50 h-full rounded-lg outline-1 flex items-center justify-center">
+            <p>Predictions will go here</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmotionDetection;
